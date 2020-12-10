3f478d485ec9c5e3464a08d7ba02e8429773afc8
Avram Twitchell <avram.twitchell@mx.com>
2020-12-10 08:33:48 -0700

    Day 5

---
+++ 2020/day5/part2.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "sort"
+)
+
+type Seat struct {
+    row int
+    col int
+}
+
+func file_to_seats(fp string) []Seat{
+    seat_strings := read_file(fp)
+    var seats []Seat
+    for _, seat_string := range seat_strings {
+        seats = append(seats, string_to_seat(seat_string))
+    }
+    return seats
+}
+
+func read_file (fp string) []string {
+    f, err := os.Open(fp)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+    var seats []string
+
+    scanner := bufio.NewScanner(f)
+
+    for scanner.Scan() {
+        seats = append(seats, string(scanner.Text()))
+    }
+
+    return seats
+}
+
+func string_to_seat(seat string) Seat {
+    row := get_row(seat)
+    col := get_col(seat)
+    return Seat{row, col}
+}
+
+func get_row(seat string) int {
+    bin := byte(0b0)
+    for _, c := range seat {
+        if string(c) == "F" {
+            bin = fl_shift(bin)
+        } else if string(c) == "B" {
+            bin = br_shift(bin)
+        }
+    }
+    return int(bin)
+}
+
+func get_col(seat string) int {
+    bin := byte(0b0)
+    for _, c := range seat {
+        if string(c) == "L" {
+            bin = fl_shift(bin)
+        } else if string(c) == "R" {
+            bin = br_shift(bin)
+        }
+    }
+    return int(bin)
+}
+
+// Add 0 if char is "F" or "L"
+func fl_shift(b byte) byte {
+	out := b
+	out = out << 1
+	return out
+}
+
+// Add 1 if char is "B" or "R"
+func br_shift(b byte) byte {
+	out := b
+	out = out << 1
+	out = out | 0b1
+	return out
+}
+
+func seat_id(seat Seat) int {
+    return (seat.row * 8) + seat.col
+}
+
+func main() {
+    seats := file_to_seats("input1.txt")
+    var seat_ids []int
+    for _, seat := range seats {
+        seat_ids = append(seat_ids, seat_id(seat))
+    }
+    sort.Ints(seat_ids)
+    last_id := 0
+    for _, seat_id := range seat_ids {
+        if last_id != seat_id - 1 {
+            fmt.Println(last_id + 1)
+        }
+        last_id = seat_id
+    }
+}
