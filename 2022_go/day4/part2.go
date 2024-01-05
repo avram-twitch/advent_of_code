08999863c8aa6d98767d0b73e8ebcc3662f7d699
avram-twitch <avram.twitch@gmail.com>
2024-01-05 14:50:46 -0700

    Day 4

---
+++ 2022_go/day4/part2.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "log"
+    "bufio"
+    "strings"
+    "strconv"
+)
+
+type Elf struct {
+    start int
+    finish int
+}
+
+func read_file(filePath string) [][]Elf {
+    file, err := os.Open(filePath)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var pairs [][]Elf
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {
+        line := scanner.Text()
+        splitLine := strings.Split(line, ",")
+
+        var elves []Elf
+
+        for _, line := range splitLine {
+            thisRange := strings.Split(line, "-")
+            first, ok := strconv.Atoi(thisRange[0])
+            if ok != nil {
+                log.Fatal(ok)
+            }
+
+            second, ok := strconv.Atoi(thisRange[1])
+
+            if ok != nil {
+                log.Fatal(ok)
+            }
+
+            elves = append(elves, Elf{start: first, finish: second})
+        }
+
+        pairs = append(pairs, elves)
+        elves = nil
+    }
+
+    return pairs
+}
+
+func (e Elf) Contains(otherElf Elf) bool {
+    return (e.start <= otherElf.start && e.finish >= otherElf.start) || (e.start <= otherElf.finish && e.finish >= otherElf.finish)
+}
+
+func main() {
+    elfPairs := read_file("input.txt")
+
+    sum := 0
+
+    for _, pair := range elfPairs {
+        containsOther := pair[0].Contains(pair[1]) || pair[1].Contains(pair[0])
+        if containsOther {
+            sum++
+        }
+    }
+
+    fmt.Printf("Result: %d", sum)
+}
